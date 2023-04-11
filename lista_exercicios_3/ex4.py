f9569bd604d441681222b77ad4c22fb85617ec33
Kauã Victor Gomes Paiva <20231370023@ad.ifpb.edu.br>
2023-04-11 13:53:50 -0300

    testando modificacao

--- lista_exercicios_3/ex4.py
+++ lista_exercicios_3/ex4.py
@@ -1,11 +1,21 @@
 nome = input('Digite seu nome: ').title()
 sexo = input('Informe seu sexo: (M)Masculino ou (F)Feminino: ')
 
-if sexo == 'M' or sexo == 'm':
-    print(f'Olá, Sr. {nome}!')
+# if sexo == 'M' or sexo == 'm':
+#     print(f'Olá, Sr. {nome}!')
 
-elif sexo == 'F' or sexo == 'f':
-    print(f'Olá, Sra. {nome}!')
+# elif sexo == 'F' or sexo == 'f':
+#     print(f'Olá, Sra. {nome}!')
 
-else:
-    print(f'{nome}, informou o sexo inadequadamente, corrija!')
+# else:
+#     print(f'{nome}, informou o sexo inadequadamente, corrija!')
+
+match sexo:
+    case 'M' | 'm':
+        print(f'Olá, Sr. {nome}!')
+
+    case 'F' | 'f':
+        print(f'Olá, Sra. {nome}!')
+
+    case _:
+         print(f'{nome}, informou o sexo inadequadamente, corrija!')
