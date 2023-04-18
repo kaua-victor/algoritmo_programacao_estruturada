fd8dabf15fe70175a2abd830a5817357d29c0c05
Kauã Victor Gomes Paiva <20231370023@ad.ifpb.edu.br>
2023-04-18 13:20:18 -0300

    atividade repeticao

---
+++ lista_exercicios_4/atv7.py
@@ -0,0 +1,13 @@
+numero = int(input('Digite um número: '))
+
+if numero <=1:
+    print('Não é primo!')
+else:
+    eh_primo = True
+    for i in range(2,numero):
+        if numero%i==0:
+            eh_primo = False
+    if eh_primo:
+        print(f'{numero} é primo!')
+    else:
+        print(f'{numero} não é primo!')
