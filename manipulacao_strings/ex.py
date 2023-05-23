0ba2b7a1e8aa77e0b58e660adb219fcffca9ac80
Kauã Victor Gomes Paiva <20231370023@ad.ifpb.edu.br>
2023-05-23 14:29:43 -0300

    Exemplos Strings

---
+++ manipulacao_strings/ex.py
@@ -0,0 +1,45 @@
+#fatiamento
+# s = 'Linguagem python'
+
+# print(s[0:9])
+
+#concatenação
+# s1 = 'ab'
+# s2 = 'xy'
+# s3 = 3*s1+s2
+# print(s3)
+
+#contagem
+# s = 'chinelo frito'
+# print(f'A letra "o" aparece: {s.count("o")}')
+
+#conversão de maiúsculas/minúsculas
+# s = input('Digite um nome: ').upper()
+# print(s)
+# s = 'nao sei'
+# print(f'{s.upper()}')
+
+#Busca
+# s = 'aula de python'
+# print(s.find('de'))
+
+#Substituição
+# s = 'Aula de Python'
+# s1  = 'Python'
+# s2 = 'Java'
+# print(f'{s.replace(s1,s2)}')
+
+#Divisão
+# frase = 'Aula de Python' 
+# divisao = frase.split()
+# print(frase)
+# print(divisao)
+# print(divisao[2])
+# nome = input('Digite seu nome: ')
+# divisao = nome.split()
+# print(f'Sobrenome: {divisao[-1]}')
+
+#Percorrendo Strings
+frase = 'Aula de Python'
+for i in frase:
+    print(i,' ',end='')
